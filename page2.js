8239d161aea4430d682dd8015174b5797c71456d
Basile Carle <basile.carle@edhec.com>
2022-10-23 20:02:05 +0200

    1ère version d'un site basique développé sur le tas incluant deux pages destinées à s'entrainer. L'une se concentre sur du html basique, la gestion de display flex, display grid, l'affiche de différents éléments via l'attribut position (fixed, absolute), l'utilisation basique de javascript pour importer un objet .json etc ...
    l'autre page se concentre sur la réalisation d'un menu hamburger, appel API REST, et différentes manipulation en JS (filtre de recherche)

---
+++ page2.js
@@ -0,0 +1,100 @@
+let simpsons;
+let count = 8;
+
+const burgerbtn = document.querySelector(".burgerbtn");
+const subnavbar = document.querySelector(".subnavbar");
+const subnavbarlist = document.querySelector(".subnavbarlist");
+const closebtn = document.querySelector("#closebtn");
+
+// load stuff at the beginning
+
+const simpsonApi = `https://thesimpsonsquoteapi.glitch.me/quotes?count=${count}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#amount").value = count;
+  fetch("./title.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const randomi = Math.floor(Math.random() * data.length);
+      console.log(randomi);
+      document.querySelector("#titre").innerHTML = data[randomi];
+    });
+  fetch("./navbar.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i in data) {
+        const item = document.createElement("li");
+        item.innerHTML =
+          "<a href='./" + data[i].link + "'>" + data[i].title + "</a>";
+        subnavbarlist.appendChild(item);
+      }
+    });
+  fetchSimpsonQuotes(count);
+  // fetch(simpsonApi)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     simpsons = data;
+  //     displaySimpsons(data);
+  //   });
+});
+
+// actions
+
+burgerbtn.addEventListener("click", () => {
+  burgerbtn.classList.toggle("clicked");
+  subnavbar.classList.toggle("active");
+  console.log(simpsons);
+});
+
+closebtn.addEventListener("click", () => {
+  subnavbar.classList.toggle("active");
+});
+
+// filter input
+
+document.querySelector("#search").addEventListener("change", () => {
+  const searchValue = document.querySelector("#search").value;
+  const filteredQuotes = simpsons.filter((character) =>
+    character.character.includes(searchValue)
+  );
+  console.log(filteredQuotes);
+  displaySimpsons(filteredQuotes);
+});
+
+// fonction display simpson's quotes
+
+const displaySimpsons = (data) => {
+  document.querySelector(".cards").innerHTML = "";
+  data.map((simpson) => {
+    const quote = simpson.quote;
+    const url = simpson.image;
+    const character = simpson.character;
+    const cardHtml =
+      '<div class="quotecard"><div class="simpson-visual"><div class="image"><img src="' +
+      url +
+      '" height="100" /></div></div><div class="quote">"' +
+      quote +
+      '"</div><div class="name">' +
+      character +
+      "</div></div>";
+    document.querySelector(".cards").innerHTML += cardHtml;
+  });
+};
+
+const amount = document.querySelector("#amount");
+amount.addEventListener("change", () => {
+  count = amount.value;
+  fetchSimpsonQuotes(count);
+});
+
+// fonction pour fetcher l'api simpson
+
+const fetchSimpsonQuotes = (count) => {
+  const simpsonApi = `https://thesimpsonsquoteapi.glitch.me/quotes?count=${count}`;
+  fetch(simpsonApi)
+    .then((response) => response.json())
+    .then((data) => {
+      simpsons = data;
+      displaySimpsons(data);
+    });
+};
